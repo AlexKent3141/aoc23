79b77ea6cb81e39316922a1a589cd44174c384c3
Alex Kent <alex.kent@ultraleap.com>
2023-12-20 10:23:22 +0000

    Skeleton for the algo. Need to figure out conjunction nodes.

--- day20/d20.cpp
+++ day20/d20.cpp
@@ -9,6 +9,8 @@
 #include <vector>
 
 enum NodeType {
+  BT,
+  BR,
   FF,
   CN
 };
@@ -17,20 +19,78 @@ struct Node {
   bool populated;
   std::string label;
   NodeType type;
+  std::vector<Node*> inputs;
   std::vector<Node*> targets;
+  bool ff_is_on;
+};
+
+struct Pulse {
+  Node* sender;
+  Node* target;
+  bool is_high;
 };
 
 int index_from_label(const std::string& label) {
   return 26 * (label[0] - 'a') + (label[1] - 'a');
 }
 
+std::pair<int, int> push_button(const Node& button) {
+  int num_low_pulses = 0;
+  int num_high_pulses = 0;
+
+  std::deque<Pulse> todo;
+
+  // Send low pulse from button to broadcaster.
+  todo.push_back(Pulse{const_cast<Node*>(&button), button.targets.front(), false});
+
+  while (!todo.empty()) {
+    const auto p = todo.front();
+    todo.pop_front();
+
+    if (p.is_high) ++num_high_pulses;
+    else ++num_low_pulses;
+
+    // Execute this pulse updating the state of the target.
+    Node* target = p.target;
+    switch (p.target->type) {
+      case BR: {
+        // Forward this pulse on to all of the broadcaster's targets.
+        for (Node* next_target : p.target->targets) {
+          todo.push_back(Pulse{target, next_target, p.is_high});
+        }
+        break;
+      }
+      case FF: {
+        if (p.is_high) break;
+
+        // Invert state and fire pulse with the new state.
+        p.target->ff_is_on = !p.target->ff_is_on;
+        for (Node* next_target : p.target->targets) {
+          todo.push_back(Pulse{target, next_target, p.target->ff_is_on});
+        }
+        break;
+      }
+      case CN: {
+        // Update the conjunction node's latest info for this input.
+        break;
+      }
+      case BT: std::abort();
+    }
+  }
+
+  return std::make_pair(num_low_pulses, num_high_pulses);
+}
+
 int main() {
   std::ifstream fs("input.txt");
   std::string line, token;
 
-  Node broadcaster;
+  Node button, broadcaster;
   std::array<Node, 26*26> nodes;
   for (auto& n : nodes) n.populated = false;
+
+  button.type = BT;
+  button.targets.push_back(&broadcaster);
 
   while (std::getline(fs, line)) {
     std::stringstream ss(line);
@@ -41,11 +101,13 @@ int main() {
     if (token[0] == 'b') {
       n = &broadcaster;
       n->label = "broadcaster";
+      n->type = BR;
     }
     else {
       n = &nodes[index_from_label(token.substr(1))];
       n->label = token.substr(1);
       n->type = token[0] == '%' ? FF : CN;
+      n->ff_is_on = false;
     }
 
     n->populated = true;
@@ -56,7 +118,9 @@ int main() {
     // Populate the targets.
     ss >> token;
     while (ss) {
-      n->targets.push_back(&nodes[index_from_label(token.substr(0, 2))]);
+      Node* target = &nodes[index_from_label(token.substr(0, 2))];
+      n->targets.push_back(target);
+      target->inputs.push_back(n);
       ss >> token;
     }
   }
