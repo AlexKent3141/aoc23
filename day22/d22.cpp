3e652d0df9ff72bdf2a948e5923dd3549e6ae0ee
Alex Kent <alex.kent@ultraleap.com>
2023-12-22 11:06:43 +0000

    Try P1: wrong answer.

--- day22/d22.cpp
+++ day22/d22.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -18,14 +19,14 @@ struct Block {
   int id;
   std::vector<Point> points;
 
-  Block(int, const Point&, const Point&);
+  Block(const Point&, const Point&);
 
   // Shift the whole block up or down.
   void lower() { for (auto& p : points) --p.z; }
   void raise() { for (auto& p : points) ++p.z; }
 };
 
-Block::Block(int id_, const Point& end1, const Point& end2) : id(id_) {
+Block::Block(const Point& end1, const Point& end2) {
   if (end1.x != end2.x) {
     // Oriented along the x axis.
     assert(end1.y == end2.y && end1.z == end2.z);
@@ -54,6 +55,7 @@ public:
   Tetris(std::vector<Block>&);
 
   void gravity();
+  int count_safe_disintegrations() const;
 
 private:
   std::vector<Block> blocks_;
@@ -72,6 +74,18 @@ Tetris::Tetris(std::vector<Block>& blocks)
     [] (const auto& b1, const auto& b2) {
       return b1.points[0].z < b2.points[0].z;
     });
+
+  // Allocate each block an ID based on its index.
+  for (std::size_t id = 0; id < blocks.size(); id++) {
+    blocks_[id].id = id;
+  }
+
+  // Fill the occupied stacks with zeros initially.
+  for (std::size_t x = 0; x < base_size; x++) {
+    for (std::size_t y = 0; y < base_size; y++) {
+      occupied_stacks_[x][y].fill(-1);
+    }
+  }
 }
 
 void Tetris::gravity() {
@@ -81,7 +95,7 @@ void Tetris::gravity() {
   const auto block_intersects = [this] (const Block& b) {
     for (const auto& p : b.points) {
       if (p.z < 1) return true; // Cannot go through the floor.
-      if (occupied_stacks_[p.x][p.y][p.z] != 0) return true;
+      if (occupied_stacks_[p.x][p.y][p.z] != -1) return true;
     }
     return false;
   };
@@ -94,14 +108,61 @@ void Tetris::gravity() {
     }
 
     b.raise();
+
+    // Flag these locations as occupied.
+    for (const auto& p : b.points) {
+      occupied_stacks_[p.x][p.y][p.z] = b.id;
+    }
   }
+}
+
+int Tetris::count_safe_disintegrations() const {
+  // A block can be safely disintegrated if its removal would not cause
+  // any blocks immediately above to fall (must have at least one other support).
+
+  // Plan: for each block, look at the blocks directly above it.
+  // If all blocks directly above have multiple blocks supporting then this block
+  // can be safely removed.
+  int count = 0;
+  for (const auto& block : blocks_) {
+    Block b(block);
+    b.raise();
+
+    std::set<int> ids_above;
+    for (const auto& p : b.points) {
+      int id = occupied_stacks_[p.x][p.y][p.z];
+      if (id == -1) continue;
+      ids_above.insert(id);
+    }
+
+    bool single_support = false;
+    for (auto id : ids_above) {
+      // Count the supports for this block.
+      Block b_above(blocks_[id]);
+      b_above.lower();
+      std::set<int> ids_below;
+      for (const auto& p : b_above.points) {
+        int id_below = occupied_stacks_[p.x][p.y][p.z];
+        if (id_below == -1) continue;
+        ids_below.insert(id_below);
+      }
+
+      if (ids_below.size() == 1) {
+        single_support = true;
+        break;
+      }
+    }
+
+    count += !single_support;
+  }
+
+  return count;
 }
 
 int main() {
   std::ifstream fs("input.txt");
   std::vector<Block> blocks;
   std::string line, token;
-  int id = 1;
   while (std::getline(fs, line)) {
     std::replace(line.begin(), line.end(), ',', ' ');
     std::replace(line.begin(), line.end(), '~', ' ');
@@ -116,7 +177,7 @@ int main() {
     ss >> token; end2.y = std::stoi(token);
     ss >> token; end2.z = std::stoi(token);
 
-    Block b(id++, end1, end2);
+    Block b(end1, end2);
     blocks.push_back(b);
   }
 
@@ -127,4 +188,7 @@ int main() {
     std::cout << b.points[0].x << ", "<< b.points[0].y << ", " << b.points[0].z << " ~ "
               << b.points.back().x << ", "<< b.points.back().y << ", " << b.points.back().z << "\n";
   }
+
+  int p1 = t.count_safe_disintegrations();
+  std::cout << "P1: " << p1 << "\n";
 }
